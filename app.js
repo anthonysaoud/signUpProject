95f3ea586456f186dd999380108053f007d2a519
Anthony Saoud <asaoud@asaouds-MacBook-Pro.local>
2022-05-18 20:56:44 -0400

    configured env

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const https = require('https')
+const sgMail = require('@sendgrid/mail')
+require('custom-env').env('.env.staging')
 const app = express()
+
 
 //storing the secret keys in this mailchimjson package
 const fs = require('fs');
-const obj = JSON.parse(fs.readFileSync('mailchimpCredentials.json', 'utf8'));
+//const obj = JSON.parse(fs.readFileSync(__dirname+'/mailchimpCredentials.json', 'utf8'));
 
 
 //this allows you to access the css and images on the server side. relative URL
@@ -45,8 +48,9 @@ app.post("/", function (req, res) {
     //convert data object into a jsonData format using stringify
     var jsonData = JSON.stringify(data)
 
-    const appid  = obj.credentials[0].appid
-    const listID = obj.credentials[0].listid
+    //const appid  = obj.credentials[0].appid
+    const appid = process.env.appid
+    const listID = '433266e95e'
     const url = "https://us13.api.mailchimp.com/3.0/lists/" + listID
     const options = {
         method: "POST",
@@ -62,11 +66,48 @@ app.post("/", function (req, res) {
             }
             console.log(JSON.parse(data));
         })
-        
-    })
+    
+    
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: email,
+      from: 'apsg2000@gmail.com', // Use the email address or domain you verified above
+      subject: 'Thanks ' +firstName+' for Signing Up to my Newsletter!',
+      text: 'Looking forward to having you be part of my journey of becoming a full fledged software engineer!',
+      html: '<strong>Looking forward to having you be part of my journey of becoming a full fledged software engineer!</strong>',
+    };
+    //ES6
+    sgMail
+      .send(msg)
+      .then(() => {}, error => {
+        console.error(error);
+    
+        if (error.response) {
+          console.error(error.response.body)
+        }
+      });
+    //ES8
+    (async () => {
+      try {
+        await sgMail.send(msg);
+      } catch (error) {
+        console.error(error);
+    
+        if (error.response) {
+          console.error(error.response.body)
+        }
+      }
+    })();
+
+
+
+
+
+
+})
     request.write(jsonData)
     request.end()
-    
 })
 
 //create route for try again button from the failure page
